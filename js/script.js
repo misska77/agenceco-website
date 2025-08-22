419c8daec4cd987a1dcd2842837da0711a1f1e73
Katia Gontier <misska.g@outlook.fr>
2025-08-22 11:10:14 +0200

    reorganisation des fichiers

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.nav-links');
+const btnToggle = document.querySelector('.toggle');
+const iconToggle = document.querySelector('.fa-bars')
+
+btnToggle.onclick = function () {
+    if (iconToggle.classList.contains("fa-bars")) {
+        iconToggle.classList.replace("fa-bars", "fa-times")
+        menu.classList.toggle('hidden')
+    } else {
+        iconToggle.classList.replace("fa-times", "fa-bars")
+        menu.classList.toggle('hidden')
+    }
+}
