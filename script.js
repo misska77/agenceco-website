99b81d7d366f96de01edbfd3291f800b6f472897
Katia Gontier <misska.g@outlook.fr>
2025-09-06 11:19:39 +0200

    gestion slide en responsive

--- script.js
+++ script.js
@@ -22,20 +22,21 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("galerieSlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+
+  if(n > slides.length) { slideIndex = 1 }
+  if(n < 1) {slideIndex = slides.length}
+
+  for(let i = 0; i< slides.length; i++) {
+    slides[i].style.display = "none"
   }
 
+  slides[slideIndex - 1].style.display = 'block'
+}
+
+// Actualité
 async function getArticles() {
   console.log("getArticles")
   let response = await fetch("http://localhost:3000/articles");
