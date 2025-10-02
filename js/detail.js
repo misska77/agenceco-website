1fe79b212d2218d9c260baca0c03e5091aa5f19f
Katia Gontier <misska.g@outlook.fr>
2025-10-02 16:48:40 +0200

    reorg

--- js/detail.js
+++ js/detail.js
@@ -1,26 +1,26 @@
-  // Récupère l'ID depuis l'URL
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get("id");
+// Récupère l'ID depuis l'URL
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
-    if (id) {
-      fetch(`http://localhost:3000/articles/${id}`)
-        .then(res => {
-          if (!res.ok) throw new Error("Article introuvable");
-          return res.json();
-        })
-        .then(article => {
-          const container = document.getElementById("detail-container");
-          container.innerHTML = `
+if (id) {
+  fetch(`http://localhost:3000/articles/${id}`)
+    .then(res => {
+      if (!res.ok) throw new Error("Article introuvable");
+      return res.json();
+    })
+    .then(article => {
+      const container = document.getElementById("detail-container");
+      container.innerHTML = `
             <h1>${article.title}</h1>
             <p>${article.content}</p>
             <p><strong>Date :</strong> ${article.publicationDate}</p>
           `;
-        })
-        .catch(err => {
-          console.error(err);
-          document.getElementById("detail-container").textContent = "Erreur : impossible de charger l'article.";
-        });
-    } else {
-      document.getElementById("detail-container").textContent = "Aucun article sélectionné.";
-    }
- 
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById("detail-container").textContent = "Erreur : impossible de charger l'article.";
+    });
+} else {
+  document.getElementById("detail-container").textContent = "Aucun article sélectionné.";
+}
+
